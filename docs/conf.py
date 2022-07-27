4d5e1c77d52d4e578db30698651c24d24cd1b00c
summersphinx <summersphinx@duck.com>
2022-07-27 10:58:27 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ author = 'Summersphinx'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst-parser']
+extensions = ['myst_parser']
 
 source_suffix = [
     ".md",
