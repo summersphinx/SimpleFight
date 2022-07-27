1730bdeb46aad4574701e154356b9e37214aed43
summersphinx <summersphinx@duck.com>
2022-07-27 10:56:42 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,12 @@ author = 'Summersphinx'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser"]
+extensions = []
+
+source_suffix = [
+    ".md",
+    ".rst",
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
